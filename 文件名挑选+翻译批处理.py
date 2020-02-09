6ea32bd3b54c9cb026648b8f0036ecb48e38d9ea
KING5490 <50351251+King5490@users.noreply.github.com>
2020-02-09 16:47:33 +0800

    optimization...

--- 文件名挑选+翻译批处理.py
+++ 文件名挑选+翻译批处理.py
@@ -18,35 +18,39 @@ def files_filter():
     target_num = 0
     failed_num = 0
     path = os.getcwd()
-    all_path = os.walk(path)
 
     folder = os.path.exists(path + '/挑选结果')
     if not folder:
         os.mkdir('挑选结果')
 
+    all_path = os.walk(path)
+    for root, dirs, files in all_path:
+        for file in files:
+            all_files.extend([file])
+
+    all_path = os.walk(path)
     for root, dirs, files in all_path:
         for file in files:
             file_name = os.path.join(root, file)
-            all_files.extend([file])
 
-            if re.search('特征正则表达式1', file) and re.search('特征正则表达式2', file):
-                command = 'move "' + file_name + '" "' + path + '/挑选结果"'
-                if os.system(command):
-                    print('\n' + file_name + ' 此文件移动失败(可能为隐藏文件)')
-                    failed_num += 1
-                target_num += 1
+            if not re.search('挑选结果', root):
+                if re.search('特征正则表达式1', file) and re.search('特征正则表达式2', file):
+                    command = 'move "' + file_name + '" "' + path + '/挑选结果"'
+                    if os.system(command):
+                        print('\n' + file_name + ' 此文件移动失败(可能为隐藏文件)')
+                        failed_num += 1
+                    target_num += 1
 
-            # 本处为自动鉴别语言,进行文件名英译中,可替换任意语言互译(相关语言简写见上文官方文档)
-            elif translator.detect(file).lang == 'jw':
-                point_before = file[0:file.find('.')]
-                point_after = file[file.find('.'):len(file)]
-                file_rename = translator.translate(point_before, dest='zh-CN').text
-                command_r = 'ren "' + file_name + '" "' + file_rename + point_after + '"'
-                os.system(command_r)
+                # 本处为自动鉴别语言,进行文件名英译中,可替换任意语言互译(相关语言简写见上文官方文档)
+                elif translator.detect(file).lang == 'jw':
+                    point_before = file[0:file.find('.')]
+                    point_after = file[file.find('.'):len(file)]
+                    file_rename = translator.translate(point_before, dest='zh-CN').text
+                    command_r = 'ren "' + file_name + '" "' + file_rename + point_after + '"'
+                    os.system(command_r)
 
     print('从' + str(len(all_files)) + '个文件中挑选' + str(target_num) +
           '个特征文件, 其中失败了' + str(failed_num) + '文件.')
-
 
 files_filter()
 os.system("pause")
