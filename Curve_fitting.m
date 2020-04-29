2d781e1a110f63a32d508408bec6363db4e70062
KING5490 <50351251+King5490@users.noreply.github.com>
2020-04-29 16:08:27 +0800

    updated

--- Curve_fitting.m
+++ Curve_fitting.m
@@ -39,20 +39,42 @@ fxxwc = abs(maxy/max(y1)) %计算一维的非线性误差
 % [A,B,C] = textread('filename',format);%读取.txt文件内容,format为读取特定参数
 % A = importdata('-pastespecial');%将当前剪贴板数据导入A
 
+% A = reshape(A1,[m,n]);%调整数据为 m*n 阶矩阵
+
+% A1 = [1 3 NaN 4 NaN NaN 5];
+% NUM = numel(A1)计算数据总个数
+% S = sum(isnan(A1));%计算缺失值数目
+% R = rmmissing(A1);%删除缺失值
+% A = standardizeMissing(A1,'indicator');%indicator为缺失值替换值
+% A = fillmissing(A1,'method');%A为补全缺失值后的数据
+%
+% TF = isoutlier(A1,'method');%TF为离群值替换为1的零矩阵,使用sum(TF)统计离群值数量
+% B = rmoutliers(A1,'method');%B为删除离群值后的数据
+% B = filloutliers(A1,'method');%B为检测并替换数据中的离群值后的数据
+%
+% B = smoothdata(A1,'method');%B为平滑后的数据
+%
+% R = rescale(A1,l,u);%数据映射到指定缩放范围 l下限(默认0),u上限(默认1)
+% N = normalize(A1,'method');%归一化数据
+
+
 % x = logspace(0,10);%对数取值
 % semilogx(x,y);%按照 x 轴的对数刻度绘制数据
 % semilogy(x,y);
 % loglog(x,y);%双对数坐标轴图
 
+% stem(x,y);%绘制离散序列数据(针状图)
+% scatter(x,y);%散点图scatter3(x,y,z)三维散点图
 % polar(theta,rho);%极坐标绘制
 % plot3();%绘制三维曲线(输入点集)
 % ezplot();%符号函数图像的绘制,ezplot3()
 
 % fplot(@(x) exp(x),[-3 0]);%二维隐函数的绘制
-% fimplicit();
+% fimplicit(f);
 % f=@(x,y,z) x.^2+y.^2-1;
 % fimplicit3(f);%三维隐函数的绘制
 % fplot3()
+
 
 % [x,y] = meshgrid(0:1:10,0:1:10);%点阵的生成
 % z = 12-x.^3-y.^3;
@@ -66,28 +88,23 @@ fxxwc = abs(maxy/max(y1)) %计算一维的非线性误差
 % figure(Name,Value);%自定义图窗属性,例如figure('name','图窗名字');
 % subplot(m,n,p)%将当前图窗划分为m×n网格,并在p子图窗后接绘图,后接等指令等,会覆盖当前前的图窗的之前内容
 
-% ****插值过滤补全数据****
-%
-% A1 = [1 3 NaN 4 NaN NaN 5];
-% A = fillmissing(A1,'method');%A为补全缺失值后的数据
-% B = filloutliers(A1,'method');%B为检测并替换数据中的离群值后的数据
-%
+% ****插值补全数据****
 %
 % 分段线性插值(使用线性插值返回一维函数在特定查询点的插入值)
 % xq=[]缺失的y1对应的x1
 % yq=interp1(x1,y1,xq,'method')%默认method为 linear (spline线性度高)
 % method: linear,nearest,next,previous,pchip,cubic,v5cubic,makima,spline
-% plot(xq,yq,'o')
+% plot(xq,yq,'o');
 %
 %
 % 三次样条插值
-% xq=[]缺失的y1对应的x1
-% yq = spline(x,y,xq)
+% xq = [];%缺失的y1对应的x1
+% yq = spline(x,y,xq);
 %
 %
 % meshgrid格式的二维网格数据的插值
-% [xq,yq] = meshgrid(0:1:10);缺失的z1对应的x1,y1
-% zq=interp2(x1,y1,z1,xq,yq,'method');默认方法为 linear
+% [xq,yq] = meshgrid(0:1:10);%缺失的z1对应的x1,y1
+% zq = interp2(x1,y1,z1,xq,yq,'method');%默认方法为 linear
 % method: linear,nearest,cubic,makima,spline
 % surf(xq,yq,zq)
 
---
+++ spectrumAnalysis.m
@@ -0,0 +1,87 @@
+clear
+close all
+format
+
+t_s = 0.01;%采样周期
+f_s=1/t_s;%采样频率(Hz)
+t_start = 0;%起始时间
+t_end = 10;%结束时间
+t = t_start : t_s : t_end;%生成采样序列
+N=numel(t)-1;%采样序列间隔数
+y = 1+2*cos(2*pi*2*t+pi*45/180)+3*cos(2*pi*5*t+75*pi/180); %生成信号序列
+
+figure(1)
+plot(t,y);
+grid on
+title('原始信号时域图');
+xlabel('t/(s)');%x轴变量名称
+ylabel('信号幅值/(mv)');%y轴变量名称
+
+figure(2)
+Y = fft(y,N); %做FFT变换
+Ayy =abs(Y); %取模
+F=([1:N]-1)*f_s/N; %换算成实际的频率值
+%plot(Ayy(1:N)); %显示的FFT模值序列
+plot(F,Ayy(1:N)); %显示FFT模值结果
+grid on
+title('FFT 模值图');
+xlabel('序列数');%x轴变量名称
+ylabel('信号幅值/(无单位)');%y轴变量名称
+
+figure(3)
+Ayy=Ayy/(N/2); %换算成实际的幅度
+Ayy(1)=Ayy(1)/2; %直流分量实际幅值
+F=([1:N]-1)*f_s/N; %换算成实际的频率值
+plot(F(1:N/2),Ayy(1:N/2)); %显示换算后的FFT模值结果
+grid on
+title('幅度-频率图');
+xlabel('频率/(Hz)');%x轴变量名称
+ylabel('信号幅值/(mv)');%y轴变量名称
+
+figure(4)
+Pyy=[1:N/2];
+for i=1:N/2
+Pyy(i)=phase(Y(i)); %计算相位
+%Pyy(i)=angle(Y(i)); %计算相位,同上
+%Pyy(i)=Pyy(i)*180/pi; %相位换算为角度
+end
+stem(F(1:N/2),Pyy(1:N/2)); %显示相位图
+
+disp('查询点频率为:');%查询并显示特定点值
+disp(Pyy(find(F== 2)));
+
+grid on
+title('相位-频率图');
+xlabel('频率/(Hz)');%x轴变量名称
+ylabel('相位/(π)');%y轴变量名称
+% ylabel('相位/(°)');%y轴变量名称
+
+
+% *******FFT图像的处理及变换********
+% Druation = t_end -t_start;  %计算采样时间
+% Sampling_points = numel(Y); %采样点数
+% t2 = F-f_s/2;
+% 
+% figure(5)
+% subplot(5,1,1);
+% plot(t,y);title('原始信号图'); %绘制原始信号图
+% 
+% subplot(5,1,2);
+% plot(F,abs(Y));title('fft transform');
+% 
+% subplot(5,1,3);
+% shift_f = abs(fftshift(Y));
+% plot(F-f_s/2,shift_f);title('shift fft transform'); %将0频率分量(直流分量)移到坐标中心
+% 
+% subplot(5,1,4);
+% plot(t2(length(t2)/2:length(t2)),shift_f(length(shift_f)/2:length(shift_f)));title('shift fft transform cut');%保留正频率部分
+% 
+% subplot(5,1,5);
+% plot(F(1:length(F)/2),abs(Y(1:length(F)/2)));title('fft cut');%截取fft结果的前半部分
+% 
+
+
+
+
+
+
